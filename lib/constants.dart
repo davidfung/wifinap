37a536a4576693a106e990efa7e3905d59159b95
davidfung <davidfung@amgcomputing.com>
2020-03-13 00:14:18 -0700

    Initial Commit

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const String appTitle = 'WiFi Nap';
+const String aboutUrl = 'https://amg99.com';
+
+const TextStyle captionStyle =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 18);
+const TextStyle bodyStyle = TextStyle(fontSize: 16);
+const TextStyle linkStyle = TextStyle(fontSize: 18, color: Colors.blue);
+
+/*
+ * Settings
+ */
+const settingNapTime = 5; // minutes
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'WiFi Nap'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
