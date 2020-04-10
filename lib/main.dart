41db8f93ee763c76649554f673b918824c5eb5e9
davidfung <davidfung@amgcomputing.com>
2020-04-10 16:37:48 -0700

    print Hello to console every minute

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import 'dart:isolate';
+
+import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:flutter/material.dart';
 
 import './widgets/wifistatus.dart';
 
-void main() => runApp(MyApp());
+void printHello() {
+  final DateTime now = DateTime.now();
+  final int isolateId = Isolate.current.hashCode;
+  print("[$now] Hello, world! isolate=$isolateId function='$printHello'");
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final int helloAlarmID = 0;
+  await AndroidAlarmManager.initialize();
+  runApp(MyApp());
+  await AndroidAlarmManager.periodic(
+      const Duration(minutes: 1), helloAlarmID, printHello);
+}
 
 class MyApp extends StatelessWidget {
   @override
