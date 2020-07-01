920ccbac91037eea60cc2441bac0b79061c0e415
davidfung <davidfung@amgcomputing.com>
2020-06-30 21:05:56 -0700

    Register plugin again
    Not sure why this piece of code keep disappearing by itself.

--- android/app/src/main/kotlin/com/amg99/wifinap/MainActivity.kt
+++ android/app/src/main/kotlin/com/amg99/wifinap/MainActivity.kt
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "com.amg99/app_retain"
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
             // Note: this method is invoked on the main thread.
             call, result ->
